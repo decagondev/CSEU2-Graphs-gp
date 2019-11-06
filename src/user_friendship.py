4325d4651ca1566e84a1cbf0ef2e8e7844b536b8
Tom <tomtarpeydev@gmail.com>
2019-11-06 11:04:07 +0000

    populate graph coded

--- src/user_friendship.py
+++ src/user_friendship.py
@@ -1,4 +1,4 @@
-
+from random import random
 class User:
     def __init__(self, name):
         self.name = name
@@ -41,11 +41,39 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
+        # loop over a range of 0 to numUsers
+        for i in range(0, numUsers):
+            # add user to the graph
+            self.addUser(f"User {i}")
 
-        # Create friendships
+        # create friendships
+
+        # Generate all friendship combinations
+        # make a list of possible friendships
+        possibleFreindships = []
+        # avoid duplicates ensuring that the first number is smaller than the second
+        
+        # loop over userID in users
+        for userID in self.users:
+            # loop over friend id in a range from user id + 1 to the lastID +1
+            for friendID in range(userID + 1, self.lastID + 1):
+                # append the tuple of (user id , friend id) to the possible friendships list
+                possibleFreindships.append((userID, friendID))
+        # shuffle the possible friendships using the random.suffle method
+        random.shuffle(possibleFreindships)
+        # create afriendships of the first x ammount of pairs in the list   
+        # X determined by the formula: numusers * avgFriendships // 2
+        # we need to divide by to as each createFriendship adds 2 friendships
+        # loop over a range to numUsers * avgFriendships // 2
+        for i in range(numUsers * avgFriendships // 2):
+            # set the friendship to possible friends at i
+            friendship = possibleFreindships[i]
+            # addfriendship of friendship[0] and friendship[1]
+            self.addFriendship(friendship[0], friendship[1])
+
+ 
 
     def getAllSocialPaths(self, userID):
         """
