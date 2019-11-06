45b915d31b3f9fcbb3120cf19c170bc3521e746b
Tom <tomtarpeydev@gmail.com>
2019-11-06 10:42:50 +0000

    user generator boilerplate

--- src/user_friendship.py
+++ src/user_friendship.py
@@ -1,1 +1,19 @@
 # we will write a simple user/friendship generator
+
+    def populateGraph(self, numUsers, avgFriendships):
+        """
+        Takes a number of users and an average number of friendships
+        as arguments
+        Creates that number of users and a randomly distributed friendships
+        between those users.
+        The number of users must be greater than the average number of friendships.
+        """
+        # Reset graph
+        self.lastID = 0
+        self.users = {}
+        self.friendships = {}
+        # !!!! IMPLEMENT ME
+
+        # Add users
+
+        # Create friendships
