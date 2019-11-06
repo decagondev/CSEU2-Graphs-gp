751fe23ef9f7c7945bcd6897b19e54d371e41137
Tom <tomtarpeydev@gmail.com>
2019-11-06 09:39:35 +0000

    island starter code

--- src/challenge2/islands.py
+++ src/challenge2/islands.py
@@ -1,1 +1,38 @@
-# island count problem
+# island count problem big hint (use a Stack)
+
+
+# data structures (stack queue etc)
+
+
+# helper functions (traversal algorithm function bft, dft etc)
+# (get neighbors, etc)
+
+
+# main island counter function
+
+def island_counter(matrix):
+    # create a visited matrix
+    pass
+
+
+if __name__ == '__main__':
+    islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
+    island_counter(islands)  # 4
+
+    islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+            [0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+            [0, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+            [0, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+            [0, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 1, 1, 0, 1, 0, 0, 1, 0]]
+
+    island_counter(islands)  # 13
